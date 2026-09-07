e5e7122d9cd3e73e4ffa8cf5da3b354a6b483bee
vtu29556-Aasrith <vtu29556@veltech.edu.in>
2026-09-07 10:12:07 +0530

    Time: 20 ms (87.8%), Space: 59.4 MB (66.98%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,16 @@
+class RecentCounter {
+    private Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        queue.offer(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        
+        return queue.size();
+    }
+}
