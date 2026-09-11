69c9dce389dd703f27b6a8a196bfcf7a099abf73
vtu29556-Aasrith <vtu29556@veltech.edu.in>
2026-09-11 14:35:31 +0530

    Time: 5 ms (83.45%), Space: 46.5 MB (31.27%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.java
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -1,92 +1,101 @@
 class MyCircularDeque {
-    private int front, rear, size, cap;
-    private int[] arr;
+
+    private int[] deque;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
 
     public MyCircularDeque(int k) {
-        arr = new int[k];
-        this.cap = k;
-        front = -1;
+        capacity = k;
+        deque = new int[k];
+
+        front = 0;
         rear = -1;
         size = 0;
     }
-    
+
     public boolean insertFront(int value) {
-        if (isFull()) return false;
-        
-        if (isEmpty()) {
-            front = rear = 0; 
-        } else if (front == 0) {
-            front = cap - 1; 
-        } else {
-            front--;       
+        if (isFull()) {
+            return false;
         }
-        
-        arr[front] = value;
+
+        // Move front backward circularly
+        front = (front - 1 + capacity) % capacity;
+
+        // If deque was empty, rear should also point here
+        if (size == 0) {
+            rear = front;
+        }
+
+        deque[front] = value;
         size++;
+
         return true;
     }
-    
+
     public boolean insertLast(int value) {
-        if (isFull()) return false;
-        
-        if (isEmpty()) {
-            front = rear = 0; 
-        } else if (rear == cap - 1) {
-            rear = 0;         
-        } else {
-            rear++;      
+        if (isFull()) {
+            return false;
         }
-        
-        arr[rear] = value;
+
+        // Move rear forward circularly
+        rear = (rear + 1) % capacity;
+
+        // If deque was empty, front should also point here
+        if (size == 0) {
+            front = rear;
+        }
+
+        deque[rear] = value;
         size++;
+
         return true;
     }
-    
+
     public boolean deleteFront() {
-        if (isEmpty()) return false;
-        
-        if (size == 1) {
-            front = rear = -1; 
-        } else if (front == cap - 1) {
-            front = 0;         
-        } else {
-            front++;          
+        if (isEmpty()) {
+            return false;
         }
-        
+
+        front = (front + 1) % capacity;
         size--;
+
         return true;
     }
-    
+
     public boolean deleteLast() {
-        if (isEmpty()) return false;
-        
-        if (size == 1) {
-            front = rear = -1;
-        } else if (rear == 0) {
-            rear = cap - 1;    
-        } else {
-            rear--;            
+        if (isEmpty()) {
+            return false;
         }
-        
+
+        rear = (rear - 1 + capacity) % capacity;
         size--;
+
         return true;
     }
-    
+
     public int getFront() {
-        if (isEmpty()) return -1;
-        return arr[front];
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return deque[front];
     }
-    
+
     public int getRear() {
-        if (isEmpty()) return -1;
-        return arr[rear];
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return deque[rear];
     }
-    
+
     public boolean isEmpty() {
         return size == 0;
     }
-    
+
     public boolean isFull() {
-        return size == cap;
+        return size == capacity;
     }
 }
