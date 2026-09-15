8912e188c09bf7d8ccd357bfdc06b87c6bfa0c70
vtu29556-Aasrith <vtu29556@veltech.edu.in>
2026-09-15 14:52:54 +0530

    Time: 2 ms (97.56%), Space: 48 MB (77.7%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,43 +1,20 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-
-        int[] count = new int[26];
-
-        // Count frequency of each task
+        int[] freq = new int[26];
         for (char task : tasks) {
-            count[task - 'A']++;
+            freq[task - 'A']++;
         }
-
-        // Find the maximum frequency
         int maxFreq = 0;
-
-        for (int freq : count) {
-            maxFreq = Math.max(maxFreq, freq);
+        for (int count : freq) {
+            maxFreq = Math.max(maxFreq, count);
         }
-
-        // Number of tasks having maximum frequency
         int maxCount = 0;
-
-        for (int freq : count) {
-            if (freq == maxFreq) {
+        for (int count : freq) {
+            if (count == maxFreq) {
                 maxCount++;
             }
         }
-
-        /*
-         * Create slots based on the most frequent task.
-         *
-         * Example:
-         * A A A, n = 2
-         *
-         * A _ _ A _ _ A
-         *
-         * (maxFreq - 1) groups, each of size (n + 1)
-         * plus the last group containing maxCount tasks.
-         */
         int result = (maxFreq - 1) * (n + 1) + maxCount;
-
-        // We cannot have fewer intervals than the number of tasks.
         return Math.max(result, tasks.length);
     }
 }
