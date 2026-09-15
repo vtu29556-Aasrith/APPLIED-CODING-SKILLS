f29ae8cb5147cd147ec9ba6f3e892db3c096db3f
vtu29556-Aasrith <vtu29556@veltech.edu.in>
2026-09-15 14:03:57 +0530

    Time: 38 ms (27.26%), Space: 94.3 MB (18.09%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,21 +1,22 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        Deque<Integer> maxDeque = new ArrayDeque<>(); 
-        Deque<Integer> minDeque = new ArrayDeque<>(); 
-        
+        Deque<Integer> maxDeque = new ArrayDeque<>();
+        Deque<Integer> minDeque = new ArrayDeque<>();
         int left = 0;
-        int maxLen = 0;
-        
+        int answer = 0;
         for (int right = 0; right < nums.length; right++) {
-            while (!maxDeque.isEmpty() && nums[maxDeque.peekLast()] < nums[right]) {
+            while (!maxDeque.isEmpty() &&
+                   nums[maxDeque.peekLast()] < nums[right]) {
                 maxDeque.pollLast();
             }
             maxDeque.offerLast(right);
-            while (!minDeque.isEmpty() && nums[minDeque.peekLast()] > nums[right]) {
+            while (!minDeque.isEmpty() &&
+                   nums[minDeque.peekLast()] > nums[right]) {
                 minDeque.pollLast();
             }
             minDeque.offerLast(right);
-            while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > limit) {
+            while (nums[maxDeque.peekFirst()] -
+                   nums[minDeque.peekFirst()] > limit) {
                 if (maxDeque.peekFirst() == left) {
                     maxDeque.pollFirst();
                 }
@@ -24,10 +25,8 @@ class Solution {
                 }
                 left++;
             }
-            
-            maxLen = Math.max(maxLen, right - left + 1);
+            answer = Math.max(answer, right - left + 1);
         }
-        
-        return maxLen;
+        return answer;
     }
 }
