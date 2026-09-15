dab7649a500cafb437b560be9dde106594bd7da9
vtu29556-Aasrith <vtu29556@veltech.edu.in>
2026-09-15 15:30:47 +0530

    Time: 0 ms (100%), Space: 42.9 MB (84.72%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+    private void inorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, result);
+        result.add(root.val);
+        inorder(root.right, result);
+    }
+}
