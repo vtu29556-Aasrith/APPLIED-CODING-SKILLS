4d93614eb6dc2b8fe9abc7e920d59cf07f0d7b63
vtu29556-Aasrith <vtu29556@veltech.edu.in>
2026-09-15 14:42:50 +0530

    Time: 6 ms (5.4%), Space: 46.7 MB (13.28%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.java
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -1,100 +1,66 @@
 class MyCircularDeque {
-
     private int[] deque;
     private int front;
     private int rear;
     private int size;
     private int capacity;
-
     public MyCircularDeque(int k) {
         capacity = k;
         deque = new int[k];
-
         front = 0;
-        rear = -1;
+        rear = 0;
         size = 0;
     }
-
     public boolean insertFront(int value) {
         if (isFull()) {
             return false;
         }
-
-        // Move front backward circularly
         front = (front - 1 + capacity) % capacity;
-
-        // If deque was empty, rear should also point here
-        if (size == 0) {
-            rear = front;
-        }
-
         deque[front] = value;
         size++;
-
         return true;
     }
-
     public boolean insertLast(int value) {
         if (isFull()) {
             return false;
         }
-
-        // Move rear forward circularly
-        rear = (rear + 1) % capacity;
-
-        // If deque was empty, front should also point here
-        if (size == 0) {
-            front = rear;
-        }
-
         deque[rear] = value;
+        rear = (rear + 1) % capacity;
         size++;
-
         return true;
     }
-
     public boolean deleteFront() {
         if (isEmpty()) {
             return false;
         }
-
         front = (front + 1) % capacity;
         size--;
-
         return true;
     }
-
     public boolean deleteLast() {
         if (isEmpty()) {
             return false;
         }
-
         rear = (rear - 1 + capacity) % capacity;
         size--;
-
         return true;
     }
-
     public int getFront() {
         if (isEmpty()) {
             return -1;
         }
-
         return deque[front];
     }
-
     public int getRear() {
         if (isEmpty()) {
             return -1;
         }
-
-        return deque[rear];
+        int index = (rear - 1 + capacity) % capacity;
+        return deque[index];
     }
-
     public boolean isEmpty() {
         return size == 0;
     }
-
     public boolean isFull() {
         return size == capacity;
     }
